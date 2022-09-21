003bc1953a35ce5d13f997002001907d6b33eadf
Julian Vega <ja.vega.g10@gmail.com>
2022-09-20 19:29:51 -0500

    Project3 added

---
+++ Project_3/my_functions.py
@@ -0,0 +1,62 @@
+from re import X
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def load_data(filename):
+    df = pd.read_csv(filename, sep=",", index_col=False)
+    df.columns = ["housesize", "rooms", "price"]
+    data = np.array(df, dtype=float)
+    plot_data(data[:,:2], data[:, -1])
+    normalize(data)
+    
+    return data[:,:2], data[:, -1]
+
+def plot_data(x, y):
+    plt.xlabel('house size')
+    plt.ylabel('price')
+    plt.plot(x[:,0], y, 'bo')
+    plt.show()
+
+def normalize(data):
+    mu = []
+    std = []
+    
+    for i in range(0,data.shape[1]-1):
+        data[:,i] = ((data[:,i] - np.mean(data[:,i]))/np.std(data[:, i]))
+        mu.append(np.mean(data[:,i]))
+        std.append(np.std(data[:, i]))
+    
+    return mu, std
+
+def h(x,theta):
+    return np.matmul(x, theta)
+
+def cost_function(x, y, theta):
+    return ((h(x, theta)-y).T@(h(x, theta)-y))/(2*y.shape[0])
+
+def gradient_descent(x, y, theta, learning_rate=0.1, num_epochs=10):
+    m = x.shape[0]
+    J_all = []
+    
+    for _ in range(num_epochs):
+        h_x = h(x, theta)
+        cost_ = (1/m)*(x.T@(h_x - y))
+        theta = theta - (learning_rate)*cost_
+        J_all.append(cost_function(x, y, theta))
+        
+    return theta, J_all 
+
+def plot_cost(J_all, num_epochs):
+    plt.xlabel('Epochs')
+    plt.ylabel('Cost')
+    plt.plot(num_epochs, J_all, 'm', linewidth = "5")
+    plt.show()
+
+def test(theta, x, mu, std):
+    x[0] = (x[0] - mu[0])/std[0]
+    x[1] = (x[1] - mu[1])/std[1]
+    
+    y = theta[0] + theta[1]*x[0] + theta[2]*x[1]
+    
+    return y
