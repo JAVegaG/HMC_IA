a5dc8d08d92aa5e4538886ef665b74a5006aceed
Julian Vega <ja.vega.g10@gmail.com>
2022-09-21 00:35:42 -0500

    Test function modified

--- Project_3/my_functions.py
+++ Project_3/my_functions.py
@@ -38,15 +38,17 @@ def plot_cost(J_all, num_epochs):
 def test(theta, x, mu, std):
 
     y = []
+    x = x.copy()
 
     for k in range(0, len(x)):
         for q in range(0, x.shape[1]):
             x[k, q] = (x[k, q] - mu[q])/std[q]
-        for h in range(0, len(theta)):
-            if h == 0:
-                aux = theta[h]
-            else:
-                aux += theta[h]*x[k,h-1]
+    
+    for h in range(0, len(theta)):
+        if h == 0:
+            aux = theta[h]
+        else:
+            aux += theta[h]*x[h, h-1]
         y.append(aux)
     
     return y
