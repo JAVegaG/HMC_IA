08f929e3f216a53e7b82e60b32362954cd0be1f8
Julian Vega <ja.vega.g10@gmail.com>
2022-09-22 10:38:39 -0500

    AdaGrad

--- Project_3/my_functions.py
+++ Project_3/my_functions.py
@@ -61,7 +61,7 @@ def AdaGrad(x, y, theta, learning_rate=0.1, num_epochs=10):
     for _ in range(num_epochs):
         h_x = np.matmul(x, theta)
         cost_ = (-2/m)*(x.T@(y - h_x))
-        delta = cost_.copy()**2
+        delta = (cost_.copy())**2
         theta -= learning_rate * cost_.copy() / (np.sqrt(delta.copy() + 1e-6))
         J_all.append(cost_function(x, y, theta))
         
